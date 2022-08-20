15b5de1865eeb1f80988215bcd09dc4d99c91802
Rafael Torreson <rafael.torreson@hotmail.com>
2022-08-19 22:09:40 -0300

    feat: lesson 1 - structure data and primitive data

---
+++ index.js
@@ -0,0 +1,57 @@
+const candidatos = [
+  {
+    nome: 'Felipe',
+    idade: Number('21'),
+    dataNacimento: new Date(),
+    status: Boolean(true),
+    skills: [
+      {
+        html: true,
+        css: true,
+        javascript: false,
+        database: false,
+      },
+    ],
+  },
+  {
+    nome: 'Rafael',
+    idade: Number('23'),
+    dataNacimento: new Date(),
+    status: Boolean(false),
+    skills: [
+      {
+        javascript: true,
+        database: true,
+        rh: false,
+        cozinhar: false,
+      },
+    ],
+  },
+  {
+    nome: String('Karol'),
+    idade: Number('29'),
+    dataNacimento: new Date(),
+    status: Boolean(true),
+    skills: [
+      {
+        javascript: false,
+        database: false,
+        rh: false,
+        cozinhar: true,
+      },
+    ],
+  },
+];
+
+const candidato = candidatos[1]
+
+console.log(`As habilidades do candidato ${candidato.nome} são : ` + JSON.stringify(candidato.skills));
+
+// [] = array || vetor
+// {} = object || JSON
+
+// type="text" = String
+// type="number" = Number
+// type="date" = Date
+// status = Booleano
+// any = any
